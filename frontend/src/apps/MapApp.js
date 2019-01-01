1f590b66f8e2a5e92e7556d846f0506da54c233a
eastrd <yjia285@student.monash.edu>
2019-01-01 23:22:19 +1100

    Add Map Application js

---
+++ frontend/src/apps/MapApp.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Layout } from "antd";
+import "antd/dist/antd.css";
+import "../styles/App.css";
+import NavBar from "../components/NavBar";
+
+const { Content } = Layout;
+
+class MapApp extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <NavBar />
+          <Content />
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default MapApp;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import DataApp from "./apps/DataApp";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import MapApp from "./apps/MapApp";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route exact path="/data" component={DataApp} />
+      <Route exact path="/" component={MapApp} />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
