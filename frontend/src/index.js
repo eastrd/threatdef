6db19f30baa1964e1d957c2b8d971f981ec147bb
eastrd <yjia285@student.monash.edu>
2019-01-01 22:17:01 +1100

    Init React Router for Multi Page Application

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/index.css";
-import App from "./components/App";
+import DataApp from "./apps/DataApp";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/data" component={DataApp} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
