5c1bd2273416414f7daccb7021994581d12d2f06
eastrd <yjia285@student.monash.edu>
2019-01-02 01:06:20 +1100

    Rename DownloadApp to ResourceApp

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import DataApp from "./apps/DataApp";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import MapApp from "./apps/MapApp";
+import ResourceApp from "./apps/ResourceApp";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route exact path="/data" component={DataApp} />
       <Route exact path="/" component={MapApp} />
+      <Route exact path="/resource" component={ResourceApp} />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
