e461ad4e27f6e6d35a8dd5c05377831eab7d909a
eastrd <yjia285@student.monash.edu>
2019-01-01 23:21:07 +1100

    Add links in navigation bar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -9,16 +9,35 @@ class NavBar extends Component {
   render() {
     return (
       <Header>
-        <Menu mode="horizontal" theme="dark" style={{ lineHeight: "60px" }}>
-          <Menu.Item>
+        <Menu
+          mode="horizontal"
+          theme="dark"
+          style={{ lineHeight: "60px" }}
+          onClick={({ key }) => {
+            switch (key) {
+              case "map":
+                window.location.href = "/";
+                break;
+              case "data":
+                window.location.href = "/data";
+                break;
+              case "download":
+                window.location.href = "/download";
+                break;
+              default:
+                console.log("Error on navbar routing.");
+            }
+          }}
+        >
+          <Menu.Item key="map">
             <Icon type="radar-chart" />
             Cyber Map
           </Menu.Item>
-          <Menu.Item>
+          <Menu.Item key="data">
             <Icon type="table" />
             Data
           </Menu.Item>
-          <Menu.Item>
+          <Menu.Item key="download">
             <Icon type="download" />
             Downloads
           </Menu.Item>
