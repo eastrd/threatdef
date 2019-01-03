c31df2261bb5cbf3d554dbaa1ca677fd6076baef
eastrd <yjia285@student.monash.edu>
2019-01-04 00:16:38 +1100

    Add "Download dictionary" feature

--- frontend/src/apps/ResourceApp.js
+++ frontend/src/apps/ResourceApp.js
@@ -4,6 +4,16 @@ import NavBar from "../components/NavBar";
 import CredTable from "../components/data/CredTable";
 import { Row, Col } from "antd";
 
+// Craft URLs for static username & password text file
+const dictionaryFile = require("../res/dictionary.zip");
+const dictionaryFileUrl =
+  window.location.protocol +
+  "//" +
+  window.location.hostname +
+  ":" +
+  window.location.port +
+  dictionaryFile;
+
 class ResourceApp extends Component {
   render() {
     return (
@@ -14,7 +24,13 @@ class ResourceApp extends Component {
             <h2>Brute Force Combinations</h2>
             <CredTable pagesize={10} secondsToWait={30} />
           </Col>
-          <Col span={12}> Under Construction... </Col>
+          <Col span={12}>
+            <h3 style={{ textAlign: "center" }}>
+              <a href={dictionaryFileUrl} download>
+                Download SSH Brute Force Dictionary
+              </a>
+            </h3>
+          </Col>
         </Row>
       </div>
     );
