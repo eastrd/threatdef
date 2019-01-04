ceee39023be0297e71dbc6e0ec3b63e9d5e2028a
eastrd <yjia285@student.monash.edu>
2019-01-04 23:57:47 +1100

    Convert NavBar into Stateless Component

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,49 +1,47 @@
-import React, { Component } from "react";
+import React from "react";
 import { Layout, Menu, Icon } from "antd";
 import "antd/dist/antd.css";
 
 const { Header } = Layout;
 
-class NavBar extends Component {
-  render() {
-    return (
-      <Header>
-        <Menu
-          mode="horizontal"
-          theme="dark"
-          style={{ lineHeight: "60px" }}
-          onClick={({ key }) => {
-            switch (key) {
-              case "map":
-                window.location.href = "/";
-                break;
-              case "data":
-                window.location.href = "/data";
-                break;
-              case "resource":
-                window.location.href = "/resource";
-                break;
-              default:
-                console.log("Error on navbar routing.");
-            }
-          }}
-        >
-          <Menu.Item key="map">
-            <Icon type="radar-chart" />
-            Cyber Map
-          </Menu.Item>
-          <Menu.Item key="data">
-            <Icon type="table" />
-            Data
-          </Menu.Item>
-          <Menu.Item key="resource">
-            <Icon type="profile" />
-            Resource
-          </Menu.Item>
-        </Menu>
-      </Header>
-    );
-  }
-}
+const NavBar = () => {
+  return (
+    <Header>
+      <Menu
+        mode="horizontal"
+        theme="dark"
+        style={{ lineHeight: "60px" }}
+        onClick={({ key }) => {
+          switch (key) {
+            case "map":
+              window.location.href = "/";
+              break;
+            case "data":
+              window.location.href = "/data";
+              break;
+            case "resource":
+              window.location.href = "/resource";
+              break;
+            default:
+              console.log("Error on navbar routing.");
+          }
+        }}
+      >
+        <Menu.Item key="map">
+          <Icon type="radar-chart" />
+          Cyber Map
+        </Menu.Item>
+        <Menu.Item key="data">
+          <Icon type="table" />
+          Data
+        </Menu.Item>
+        <Menu.Item key="resource">
+          <Icon type="profile" />
+          Resource
+        </Menu.Item>
+      </Menu>
+    </Header>
+  );
+};
 
 export default NavBar;
